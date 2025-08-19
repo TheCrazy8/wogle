6ff1bfe938a65f2b5e3ab97ba23b2bc0246ee620
TheCrazy8 <bennettbashford@gmail.com>
2025-08-19 16:16:14 -0400

    Add files via upload

---
+++ dicegame.py
@@ -0,0 +1,22 @@
+import random
+
+def dice_roll():
+    return random.randint(1, 6)
+
+def play_game():
+    if input("Roll: (just hit enter)") == "":
+        num = dice_roll()
+        print(f"You rolled a {num}.")
+        if num == 1:
+            print("You lose!")
+            if input("Play again?: (just hit enter)") == "":
+                play_game()
+        elif num == 6:
+            print("You win!")
+            if input("Play again?: (just hit enter)") == "":
+                play_game()
+        else:
+            print("Roll again!")
+            play_game()
+
+play_game()
