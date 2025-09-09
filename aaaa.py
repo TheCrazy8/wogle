66611c259178fd6dd93a23c285e541aba250d11c
Ratman82 <lukasdellaveryquinn@yahoo.com>
2025-09-09 16:23:51 -0400

    a

---
+++ aaaa.py
@@ -0,0 +1,48 @@
+#Brainrot 8 ball
+import random
+x=input("Do you want to roll? Y/N ")
+while x == 'Y':
+    question=input("Enter a question: ")
+    awnser=random.randint(1,20)
+    if awnser == 1:
+        print("Heck yeah twin")
+    elif awnser == 2:
+        print("Heck nah twin")
+    elif awnser == 3:
+        print("Skibidi nahhhhhhh")
+    elif awnser == 4:
+        print("Ur cooked")
+    elif awnser == 5:
+        print("No cap u dalulu")
+    elif awnser == 6:
+        print("Gen X wont be rising for that, twin")
+    elif awnser == 7:
+        print("MOOON BEAM ICE CREAM")
+    elif awnser == 8:
+        print("Labudont")
+    elif awnser == 9:
+        print("DObai chocolate")
+    elif awnser == 10:
+        print("DONTbai chocolate")
+    elif awnser == 12:
+        print("Matcha core?")
+    elif awnser == 13:
+        print("Gimme a min im popping off")
+    elif awnser == 14:
+        print("Ts pmo")
+    elif awnser == 15:
+        print("icl ts cringe")
+    elif awnser == 16:
+        print("Im eating the bugs in my skin")
+    elif awnser == 17:
+        print("We are gen z and we ARE trying to cancel eminem")
+    elif awnser == 18:
+        print("Afk")
+    elif awnser == 19:
+        print("Assasinating JFK rq")
+    elif awnser == 20:
+        print("Deltarune tmrw")
+    x=input("Do you want to roll? Y/N ")
+
+    
+
