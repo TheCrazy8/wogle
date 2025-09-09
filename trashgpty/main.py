b94b0101e3b14c4e4ee33ee48035e36e6097e761
TheCrazy8 <bennettbashford@gmail.com>
2025-09-09 16:28:50 -0400

    ss

---
+++ trashgpty/main.py
@@ -0,0 +1,15 @@
+import ollama
+def main():
+    # Send a chat message to a specified model
+    response = ollama.chat(model='llama2', messages=[
+        {
+            'role': 'user',
+            'content': input("Enter your message: "),
+        },
+    ])
+
+    # Print the model's response
+    print(response['message']['content'])
+
+if __name__ == "__main__":
+    main()
