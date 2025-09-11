e974846c91b94a5e34f3825b2126daf74b3e93ee
TheCrazy8 <bennettbashford@gmail.com>
2025-09-11 15:54:03 -0400

    :water:

---
+++ trashgpty/mainwatereddown.py
@@ -0,0 +1,22 @@
+import ollama
+from ollama import chat
+from ollama import ChatResponse
+
+ollama.pull('gemma3')
+
+def main():
+    msg = input("Enter your message: ")
+
+    response: ChatResponse = chat(model='gemma3', messages=[
+        {
+            'role': 'user',
+            'content': msg,
+        },
+    ])
+    print(response['message']['content'])
+    # or access fields directly from the response object
+    print(response.message.content)
+
+    main()
+
+main()
