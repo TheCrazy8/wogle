bafb6f770b88e8429876c616cf2a5e1bd20d4c1d
Ratman82 <lukasdellaveryquinn@yahoo.com>
2025-09-11 15:48:43 -0400

    graver everger trm

--- grade avenger/grade averager.py
+++ grade avenger/grade averager.py
@@ -2,3 +2,15 @@
 
 x= input("How many grades do you want to enter in to the database? ")
 gradecount=int(x)
+total=0
+
+for i in range(0,gradecount):
+    y=input("Enter the grade you got on the assignment out of 100 ")
+    y2=float(y)
+    total+=y2
+average=total/gradecount
+print(f"Your grade average is {average}")
+if average < 50:
+    print("Wow you failed and you are a failure")
+else:
+    print("Wow you're not a failure and passed")
