487eeb9b4f3b24948eceb9c76a6c7617816f6c90
TheCrazy8 <bennettbashford@gmail.com>
2025-09-11 15:10:33 -0400

    s

--- trashgpty/main.py
+++ trashgpty/main.py
@@ -3,11 +3,12 @@ import ollama
 ollama.pull('llama2')
 name = "__main__"
 def main():
+    msg = input("Enter your message: ")
     # Send a chat message to a specified model
     response = ollama.chat(model='llama2', messages=[
         {
             'role': 'user',
-            'content': input("Enter your message: "),
+            'content': msg,
         },
     ])
 
