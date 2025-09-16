ff5e4405c25aa8a37188cb242785eee1bf9dac62
TheCrazy8 <bennettbashford@gmail.com>
2025-09-16 15:29:15 -0400

    ss

---
+++ osai.py
@@ -0,0 +1,11 @@
+example_one = ["apple", "banana", "cherry", "date", "fig", "grape"]
+
+print(example_one[1])
+
+for fruit in example_one:
+    print(fruit)
+
+example_one.insert(4, "elderberry")
+print(example_one)
+while True:
+    pass
